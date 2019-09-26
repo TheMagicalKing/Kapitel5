f3501023e7b68a8c7a92910416be21ac5a6640e3
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-26 11:33:59 +0200

    Update Opgave1.java

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -5,7 +5,7 @@ public class Opgave1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the examiner");
-        System.out.println("Please enter your exam score, to stop pres -1");
+        System.out.println("Please enter your exam score, to stop press -1");
         int score = input.nextInt();
         int max = 100;
         int min = 0;
@@ -13,12 +13,12 @@ public class Opgave1 {
 
             if (score > 59  && score < max){
                 System.out.println("You pass the exam");
-                System.out.println("Please enter your exam score, to stop pres -1");
+                System.out.println("Please enter your exam score, to stop press -1");
                 score = input.nextInt();
             }
             else if (score > min && score < 60) {
                 System.out.println("You fail the exam");
-                System.out.println("Please enter your exam score, to stop pres -1");
+                System.out.println("Please enter your exam score, to stop press -1");
                 score = input.nextInt();
             }
 
