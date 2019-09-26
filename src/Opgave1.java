5c8c8e947589f36ba43f4edd19ed28350b314115
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2019-09-26 11:30:21 +0200

    Initial commit

---
+++ src/Opgave1.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Opgave1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the examiner");
+        System.out.println("Please enter your exam score, to stop pres -1");
+        int score = input.nextInt();
+        int max = 100;
+        int min = 0;
+        while (score != -1){
+
+            if (score > 59  && score < max){
+                System.out.println("You pass the exam");
+                System.out.println("Please enter your exam score, to stop pres -1");
+                score = input.nextInt();
+            }
+            else if (score > min && score < 60) {
+                System.out.println("You fail the exam");
+                System.out.println("Please enter your exam score, to stop pres -1");
+                score = input.nextInt();
+            }
+
+        }
+
+
+
+
+        }
+
+    }
+
